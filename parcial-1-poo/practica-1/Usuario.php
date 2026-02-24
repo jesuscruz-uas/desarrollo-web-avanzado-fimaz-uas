11ac48d0850aad57346069b0f0bcd0712b33d322
jesuscruz-uas <chuchocruzm@gmail.com>
2026-02-23 22:11:18 -0700

    Ajuste de errores

--- parcial-1-poo/practica-1/Usuario.php
+++ parcial-1-poo/practica-1/Usuario.php
@@ -3,26 +3,26 @@
 class Usuario {
   private $nombre; 
   private $correo; 
-                                                            //Constructor 
+    //Constructor 
 public function __construct($nombre, $correo) {
-  $this->nombre = nombre;
-  $this->correo = correo;
+  $this->nombre = $nombre;
+  $this->correo = $correo;
 }
-                                                          //Metodos getter
+     //Metodos getter
 public function getNombre() {
-  return this->nombre; 
+  return $this->nombre; 
 }
 
 public function getCorreo(){
-  return this->correo;
+  return $this->correo;
 }
 
-                                                        //metodo setter
+    //metodo setter
 public function setNombre($nombre) {
   $this->nombre = $nombre;
 }
 
 public function setCorreo($correo) { 
-  this->correo = $correo; 
+ $this->correo = $correo;
   }
 }
